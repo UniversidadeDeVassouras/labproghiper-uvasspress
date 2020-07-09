45583652bcc26a63a80c5d411fa914c0cbc27069
Vinicius Almeida <vinihenriqueh@gmail.com>
2020-07-08 23:07:43 -0300

    Feat: criado gets Author
    criado gets e definições do author

--- application/model/entity/autor.py
+++ application/model/entity/autor.py
@@ -5,3 +5,22 @@ class Autor:
         self._login = login
         self._senha = senha
         self._email = email
+
+
+    def set_id (self, id):
+        self._id = id
+
+    def get_id (self):
+        return self._id
+
+    def get_nomeCompleto (self):
+        return self._nome_completo
+    
+    def get_login (self):
+        return self._login
+
+    def get_password (self):
+        return self._senha
+
+    def get_email (self):
+        return self._email
