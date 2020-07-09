3d573089156e79e97990a04d4f6c11dac6201c5d
Guilherme <guilhermedms@hotmail.com>
2020-07-09 19:34:13 -0300

    Feat: Cadastro e Login
    Foi adicionado as funcionalidades de se fazer cadastro e login

--- application/controller/login_controller.py
+++ application/controller/login_controller.py
@@ -1,15 +1,26 @@
 from application import app
-from flask import render_template, request
-from application.model.dao.usuarioDAO import UsuarioDAO
+from flask import render_template, request, current_app
 import os
 
 
+@app.route('/logado', methods = ['POST'])
+def logado():
+    usuario = current_app.config['usuario_dao']
+    email = request.form['login-email']
+    senha = request.form['login-password']
+    logar_usuario = usuario.logar_usuario(email,senha)
+    if  logar_usuario == True:
+        return render_template("logado.html")
+    else: 
+        return render_template('login.html')
+
 @app.route ('/login/cadastro', methods = ['POST'])
 def cadastro ():
+    usuario = current_app.config['usuario_dao']
     email = request.form['signup-email']
     senha = request.form['signup-password']
     confirmar_senha = request.form['signup-password-confirm']
-    cadastrar_usuario = UsuarioDAO.cadastrar_usuario(email, senha, confirmar_senha)
+    cadastrar_usuario = usuario.cadastrar_usuario(email, senha, confirmar_senha)
     return render_template('login.html', cadastrar_usuario = cadastrar_usuario)
 
 @app.route('/login')
--- application/model/dao/usuarioDAO.py
+++ application/model/dao/usuarioDAO.py
@@ -1,17 +1,18 @@
-from application import app
 from application.model.entity.autor import Autor
-
 class UsuarioDAO:
     def __init__ (self):
-        self._users = []
-        self._users.append(Autor(1, "igorbrownramos@gmail.com", "123456"))
-        self._users.append(Autor(2, "vinicius@gmail.com", "123456"))
-        
-    def cadastrar_usuario (self, email, senha):
+        self._users  = [Autor(1, "igorbrownramos@gmail.com", "123456"),Autor(2, "vinicius@gmail.com", "123456") ]
+         
+    def cadastrar_usuario (self, email, senha, confirmar_senha):
         for autor in self._users:
             if autor.get_email() == email:
                 return "Email Já Cadastrado"
-        if senha == senha:
+        if senha == confirmar_senha:
             self._users.append(Autor(len(self._users), email, senha))
             return "Cadastro Efetuado"
-        
+
+    def logar_usuario(self, email, senha):
+        for autor in self._users:
+            if autor.get_email() != email and autor.get_password() != senha:
+                return False
+        return True
