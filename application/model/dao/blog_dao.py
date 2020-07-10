96d515377bd1cb1c86e771bcd269233bc0e41a98
Lucas Leal <leallucasfonseca@gmail.com>
2020-07-09 23:22:18 -0300

    Feat: Criação blog dao
    Foram criadas as funções de acordo com os requisitos.

---
+++ application/model/dao/blog_dao.py
@@ -0,0 +1,20 @@
+from application.model.entity.blog import Blog
+
+class BlogDAO():
+    def __init__(self):
+        blog1 = Blog(1, "blog", "autor", "categoria")
+
+    def adicionar_post(self, blog, post):
+        blog.get_lista_posts().append(post) 
+        return
+
+    def alterar_status_post(self, post):  #InativarPost
+        if post.get_status() == True:
+            post.set_status(False) #Inativo
+        else:
+            post.set_status(True)  #Ativo
+            
+    def apagar_post(self, blog, post):
+        post.get_id() -= ponteiro #Pega a posição do post na lista
+        blog.get_lista_posts().pop(ponteiro) #Apaga o post
+        return
