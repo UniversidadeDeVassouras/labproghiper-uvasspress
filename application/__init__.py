74357632c7bb2d01bda4822d610aad7f860554fe
Pedro Quintella <pedro.q2000@outlook.com>
2020-07-17 16:46:49 -0300

    Feat: Criação páginas de solicitação e alteração de Senha
    Foram criadas as páginas de solicitação de recuperação de Senha e de alteração de Senha. Feitos os Controllers dessas páginas e realizados os imports destes no '__init__.py'. Foram adicionados os arquivos Bootstrap de estilização nas pastas devidas dentro do 'static'.

--- application/__init__.py
+++ application/__init__.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template
 import os
 
+
 app = Flask(__name__, static_folder=os.path.abspath('application/view/static'), template_folder=os.path.abspath('application/view/template'))
 
+
+
 from application.controller import a_controller
+from application.controller import recuperacaoSenha_controller
+from application.controller import alteracaoSenha_controller
